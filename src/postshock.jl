ba3f9019c99646d8eeafde75bd8b7987e055afda
Niclas Garan <niclas_hanraths@web.de>
2021-07-01 13:05:25 +0200

    additional methods for CJ calculations

--- src/postshock.jl
+++ src/postshock.jl
@@ -291,7 +291,19 @@ function CJ_calc(gas, gas1, ERRFT, ERRFV, x)
 end
 
 
-function CJspeed(P1, T1, q, mech; fullOutput=false)
+function CJspeed(P1, T1, q, mech; method = "CEA", kwargs...)
+    """ defaults to CEA Algorithm """
+    if method == "CEA"
+        _CJspeed_CEA(P1, T1, q, mech)
+    elseif method == "umin"
+        _CJspeed_umin(P1, T1, q, mech; kwargs...)
+    elseif method == "aeq"
+        _CJspeed_aeq(P1, T1, q, mech)
+    end
+end
+
+
+function _CJspeed_umin(P1, T1, q, mech; fullOutput=false)
     """
     Calculates CJ detonation velocity for a given pressure, temperature, and
     composition.
@@ -332,7 +344,7 @@ function CJspeed(P1, T1, q, mech; fullOutput=false)
     gas.TPX  = T1, P1, q
     gas1.TPX = T1, P1, q
     #INITIALIZE ERROR VALUES & CHANGE VALUES
-    ERRFT = 1.0*10^-4;  ERRFV = 1.0*10^-4
+    ERRFT = 1e-4;  ERRFV = 1e-4
 
     T1 = gas1.T; P1 = gas1.P
     counter = 1; R2 = 0.0; cj_speed = 0.0
@@ -604,5 +616,350 @@ function shk_eq_calc(U1, gas, gas1, ERRFT, ERRFV)
     return gas
 end
 
+
+function _CJspeed_aeq(P1, T1, q, mech)
+
+    """
+
+    CJspeed_aeq
+    Calculates CJ detonation velocity and CJ state based on equilibrium sound speed
+
+    FUNCTION
+    SYNTAX
+    cj_speed,gas = CJspeed(P1,T1,q,mech)
+
+    INPUT
+    P1 = initial pressure (Pa)
+    T1 = initial temperature (K)
+    q = string of reactant species mole fractions
+    mech = cti file containing mechanism data (i.e. 'gri30.cti')
+
+    OUTPUT
+    cj_speed = CJ detonation speed (m/s)
+
+    """
+
+    gas2 = ct.Solution(mech)
+    gas1 = ct.Solution(mech)
+    gas  = ct.Solution(mech)
+
+    #INTIAL CONDITIONS
+    gas.TPX = T1, P1, q
+    gas1.TPX = T1, P1, q
+
+    #INITIALIZE ERROR VALUES & CHANGE VALUES
+    ERRFT = 1e-4;  ERRFV = 1e-4
+
+    r1 = gas1.density; V1 = 1/r1
+    P1 = gas1.P; T1 = gas1.T
+    i = 0
+    #PRELIMINARY GUESS
+    Vg = V1/5; rg = 1/Vg
+
+    gas.TD = T1,rg
+    gas.equilibrate("UV")
+    Tg = gas.T
+    gas2.TDX = Tg, rg, gas.X
+
+    #SAVE STATE
+    V = Vg; r = rg
+    T = Tg
+    deltaT = 1000; deltaV = 1000; cj_speed = 0
+    #START LOOP
+    while (abs(deltaT) > ERRFT*T) || (abs(deltaV) > ERRFV*V)
+        i += 1
+        if i == 500
+            println("CJspeed_sound did not converge")
+            return
+        end
+
+        #CALCULATE FH & FP FOR GUESS 1
+        FH,FP,cj_speed = FHFP_CJ2(gas,gas1,gas2)
+
+        #TEMPERATURE PERTURBATION
+        DT = T*0.01; Tper = T + DT
+        Vper = V; Rper = 1/Vper
+
+        gas.TD = Tper, Rper
+        gas.equilibrate("TV")
+        gas2.TDX = Tper, Rper, gas.X
+
+        #CALCULATE FHX & FPX FOR "IO" STATE
+        FHX,FPX,cj_speed = FHFP_CJ2(gas,gas1,gas2)
+        #ELEMENTS OF JACOBIAN
+        DFHDT = (FHX-FH)/DT; DFPDT = (FPX-FP)/DT
+
+        #VOLUME PERTURBATION
+        DV = 0.01*V; Vper = V + DV
+        Tper = T; Rper = 1/Vper
+
+        gas.TD = Tper, Rper
+        gas.equilibrate("TV")
+        gas2.TDX = Tper, Rper, gas.X
+
+        #CALCULATE FHX & FPX FOR "IO" STATE
+        FHX,FPX,cj_speed = FHFP_CJ2(gas,gas1,gas2)
+        #ELEMENTS OF JACOBIAN
+        DFHDV = (FHX-FH)/DV; DFPDV = (FPX-FP)/DV
+
+        #INVERT MATRIX
+        J = DFHDT*DFPDV - DFPDT*DFHDV
+        b = DFPDV, -DFHDV, -DFPDT, DFHDT
+        a = -FH, -FP
+        deltaT = (b[1]*a[1]+b[2]*a[2])/J; deltaV = (b[3]*a[1]+b[4]*a[2])/J
+
+        #CHECK & LIMIT CHANGE VALUES
+        #TEMPERATURE
+        DTM = 0.2*T
+        if abs(deltaT) > DTM
+            deltaT = DTM*deltaT/abs(deltaT)
+        end
+        #VOLUME
+        V2X = V + deltaV
+        if V2X > V1
+            DVM = 0.5*(V1 - V)
+        else
+            DVM = 0.2*V
+        end
+        if abs(deltaV) > DVM
+            deltaV = DVM*deltaV/abs(deltaV)
+        end
+        #MAKE THE CHANGES
+        T += deltaT; V += deltaV; r = 1/V
+        gas.TD = T, r
+        gas.equilibrate("TV")
+        gas2.TDX = T, r, gas.X
+    end
+
+    FH,FP,cj_speed = FHFP_CJ2(gas,gas1,gas2)
+
+    return cj_speed
+end
+
+
+function FHFP_CJ2(gas,gas1,gas2)
+
+    """
+
+    FHFP_CJ2
+    Uses the momentum and energy conservation equations and the equilibrium sound speed to calculate error in current pressure and enthalpy guesses.  In this case, state 2 is in equilibrium.
+
+    FUNCTION
+    SYNTAX
+    [FH,FP,cj_speed] = FHFP_CJ2(gas,gas1,gas2)
+
+    INPUT
+    gas = working gas object
+    gas1 = gas object at initial state
+    gas2 = dummy gas object (for calculating numerical derivatives)
+
+    OUTPUT
+    FH,FP = error in enthalpy and pressure
+    cj_speed = CJ detonation speed (m/s)
+
+    """
+
+    P1 = gas1.P
+    H1 = gas1.enthalpy_mass
+    r1 = gas1.density
+    P2 = gas.P
+    H2 = gas.enthalpy_mass
+    r2 = gas.density
+
+    speeds = equilSoundSpeeds(gas2)
+    w2s = speeds[1]^2
+    w1s = w2s*(r2/r1)^2
+    FH = H2 + 0.5*w2s - (H1 + 0.5*w1s)
+    FP = P2 + r2*w2s - (P1 + r1*w1s)
+    return FH, FP, sqrt(w1s)
+end
+
+function equilSoundSpeeds(gas)
+
+    """
+
+    equilSoundSpeeds
+    Calculates equilibrium and frozen sound speeds. For the equilibrium sound speed, the gas is equilibrated holding entropy and specific volume constant.
+
+    FUNCTION
+    SYNTAX
+    [aequil,afrozen] = equilSoundSpeeds(gas)
+
+    INPUT
+    gas = working gas object (modified inside function)
+
+    OUTPUT
+    aequil = equilibrium sound speed (m/s)
+    afrozen = frozen sound speed (m/s)
+
+    """
+
+    # set the gas to equilibrium at its current T and P
+    gas.equilibrate("TP")
+
+    # save properties
+    s0 = gas.entropy_mass
+    p0 = gas.P
+    r0 = gas.density
+
+    # perturb the density
+    r1 = r0*1.0001
+
+    # set the gas to a state with the same entropy and composition but
+    # the perturbed density
+    gas.SV = s0, 1.0/r1
+
+    # save the pressure for this case for the frozen sound speed
+    pfrozen = gas.P
+
+    # now equilibrate the gas holding S and V constant
+    gas.equilibrate("SV")
+
+    p1 = gas.P
+
+    # equilibrium sound speed
+    aequil = sqrt((p1 - p0)/(r1 - r0))
+
+    # frozen sound speed
+    afrozen = sqrt((pfrozen - p0)/(r1 - r0));
+    return aequil, afrozen
+end
+
+function _CJspeed_CEA(P1,T1,X1,mech)
+    """
+    This function calculates the CJ-detonation velocity for given initial
+    conditions.
+
+    It is based on (i.e. mostly copied from) the CJ-detonation part of the
+    NASA CEA Fortran95 code, available at
+    http://www.grc.nasa.gov/WWW/CEAWeb/ceaHome.htm,
+    which itself is based on "Calculation of Detonation Properties and Effect
+    of Independent Parameters on Gaseous Detonations" by Frank J. Zeleznik
+    and Sanford Gordon, ARS Journal, April 1962.
+
+    INPUT:
+    P1 = pressure in (Pa)
+    T1 = temperature in (K)
+    X1 = mole fraction column vector
+    mech = file name of kinetic mechanism, e.g. 'gri30.xml'
+
+    OUTPUT:
+    cj_speed = CJ detonation velocity in (m/s)
+    """
+
+
+    gas = ct.Solution(mech)
+    gas.TPX = T1,P1,X1
+
+
+    M1 = gas.mean_molecular_weight
+    h1 = gas.enthalpy_mass
+
+    P = 15 * P1
+
+    # Acquire initial guess for T2 by finding flame temperature according to
+    # chapter 8.3 of CEA documentation
+    h_init = h1 + 0.75 * R̄ * T1/M1 * P/P1
+    gas.HP = h_init, P
+    gas.equilibrate("HP")
+    T_init = gas.T
+    T = T_init
+
+    # Further improving initial guesses for T and P by recursion formulas
+    for i = 1:3
+        gas.TPX = T,P,X1
+        gas.equilibrate("TP")
+        M2 = gas.mean_molecular_weight
+        α = T1/T * M2/M1
+        γₛ, cₚ_eq, Dlvtp, Dlvpt = partDeriv(T,P,X1,gas)
+        P = P1 * (1+γₛ) / (2*γₛ*α) * (1 + (1- (4*γₛ*α)/(1+γₛ)^2)^0.5)
+        r = α * P/P1
+        T = T1 * T_init/T1 - 0.75 * R̄/(M1*cₚ_eq) * 15 + (R̄*γₛ)/(2*M1*cₚ_eq) * (r^2-1)/r * P/P1
+    end
+
+    # Main iteration loop
+    cj_speed = 0
+    I = 0
+    while true
+        I += 1
+        gas.TPX = T,P,X1
+        gas.equilibrate("TP")
+        M2 = gas.mean_molecular_weight
+        γₛ, cₚ_eq, Dlvtp, Dlvpt = partDeriv(T,P,X1,gas)
+        α = T1/T * M2/M1
+        r = α * P/P1
+        a₁₁ = P1/P + γₛ*r*Dlvpt
+        a₁₂ = γₛ*r*Dlvtp
+        a₂₁ = 0.5*γₛ*(r^2 - 1 - Dlvpt*(1+r^2)) + Dlvtp - 1
+        a₂₂ = -0.5*γₛ*Dlvtp*(r^2+1) - M2*cₚ_eq/R̄
+        b₁ = P1/P - 1 + γₛ*(r-1)
+        b₂ = M2*(gas.enthalpy_mass-h1)/(R̄*T) - 0.5*γₛ*(r^2-1)
+        d = a₁₁*a₂₂ - a₁₂*a₂₁
+        x₁ = (a₂₂*b₁-a₁₂*b₂)/d
+        x₂ = (a₁₁*b₂-a₂₁*b₁)/d
+
+        δ = 1
+        temp = maximum((abs(x₁), abs(x₂)))
+        if temp > 0.4054652
+            δ = 0.4054652/temp
+        end
+        P = P*exp(x₁*δ)
+        T = T*exp(x₂*δ)
+        soundspeed_equil = sqrt(R̄*γₛ*T/M2)
+        cj_speed = r*soundspeed_equil
+
+        # Convergence test
+        if I > 8
+            println("Could not converge in 8 iterations")
+            break
+        elseif I <= 8 && temp < 0.5e-4
+            break
+        end
+    end
+    return cj_speed
+end
+
+function partDeriv(T,P,X,gas)
+    """
+    Calculates all needed variables related to partial derivatives, i.e
+    γₛ (isentropic exponent), cₚ_eq (equilibrium specific heat at
+    constant pressure), Dlvtp (d ln(v) / d ln(T) at constant pressure),
+    Dlvpt (d ln(v) / d ln(P) at constant temperature). Derivatives are
+    approximated via central differences.
+    """
+
+    v = 1/gas.density
+    ΔT = T*1e-4
+    ΔP = P*1e-4
+
+    v_P = zeros(2)
+    v_T = zeros(2)
+    h = zeros(2)
+
+    sign = -1, 1
+
+    for i = 1:2
+
+        gas.TPX = T, P + sign[i]*ΔP, X
+        gas.equilibrate("TP")
+        v_P[i] = 1/gas.density
+
+        gas.TPX = T + sign[i]*ΔT, P, X
+        gas.equilibrate("TP")
+        v_T[i] = 1/gas.density
+        h[i] = gas.enthalpy_mass
+    end
+
+    Dlvtp = T/v * (v_T[2] - v_T[1]) / (2*ΔT)
+    Dlvpt = P/v * (v_P[2] - v_P[1]) / (2*ΔP)
+    cₚ_eq = (h[2]-h[1])/(2*ΔT)
+
+    gas.TPX = T,P,X
+    gas.equilibrate("TP")
+
+    γₛ = -1/(Dlvpt + R̄ * Dlvtp^2 / (cₚ_eq * gas.mean_molecular_weight))
+
+    return γₛ, cₚ_eq, Dlvtp, Dlvpt
+end
 
 end #module
